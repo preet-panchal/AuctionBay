8040c7f46febbc3b441cf109eae360657eb6c332
Preet Panchal <panchalpreet094@gmail.com>
2023-02-07 16:41:00 -0500

    auctionsystem main menu

--- PHASE-2/src/AuctionSystem.cpp
+++ PHASE-2/src/AuctionSystem.cpp
@@ -8,26 +8,22 @@ using namespace std;
 
 int main () {
 
-    cout << "Welcome to AuctionBay!\n--\n";
-    cout << "Here is a list of operations (what to input):\n";
-    cout << "- Login (login)\n
-            - Create (create)\n
-            - Delete (delete)\n
-            - Advertise (advertise)\n
-            - Bid (bid)\n
-            - Refund (refund)\n
-            - Add Credit (addcredit)\n
-            - List All Items (listall)\n
-            - Logout (logout)\n\n";
+    cout << "Welcome to AuctionBay!\n--" << endl;
+    cout << "Here is a list of operations (what to input):" << endl;
+    cout << "- Login (login)\n" 
+         << "- Create (create)\n"
+         << "- Delete (delete)\n"
+         << "- Advertise (advertise)\n"
+         << "- Bid (bid)\n"
+         << "- Refund (refund)\n"
+         << "- Add Credit (addcredit)\n"
+         << "- List All Items (listall)\n"
+         << "- Logout (logout)\n" << endl;
 
+    string user_input;
     do {
-
-        string user_input;
-
-        printf("Enter operation:");
-
+        printf("Enter operation: ");
         cin >> user_input;
-
         printf("\ntest\n");
     } while (user_input != "logout");
 };
