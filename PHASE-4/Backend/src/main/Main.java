bdbdd71673c48f9f5e1cfb23c9790495900801f3
Eihab <eihabsyed@gmail.com>
2023-03-17 02:25:09 -0400

    Phase 4 final push

--- PHASE-4/Backend/src/main/Main.java
+++ PHASE-4/Backend/src/main/Main.java
@@ -1,10 +1,81 @@
+/* Program: AuctionBay
+ * Authors: Rija Baig, Preet Panchal, Eihab Syed, Nathaniel Tai
+ * Description: Auction-style Sales Service -- Backend Application.
+*/
+
+// File input (AuctionBay/PHASE-4/Backend/): daily_transaction.txt, available_items.txt, current_user_accounts.txt
+// File output (AuctionBay/PHASE-4/): new_available_items.txt, new_current_user_accounts.txt
+
 package main;
 
-public class Main {
+import java.io.*; 
 
-    public static void main(String[] args) {
-        String transFile = "daily_transaction.txt";
-        FileController fc = new FileController();
-        fc.load(transFile);
-    }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String mergedTransFile = "daily_transaction.txt";
+        String oldUserAccFile = "current_users_accounts.txt";
+        String oldAvailItemsFile = "available_items.txt";
+        
+        // open current_users_accounts file for reading
+        UserManager userManager = new UserManager();
+		userManager.load(oldUserAccFile);
+        userManager.displayUsers(); 
+		
+        // open available_items file for reading
+		Auction auction = new Auction();
+		auction.load(oldAvailItemsFile);
+		auction.displayAuctions();
+        
+        // store each individual transaction
+        String transaction;
+        
+        try {
+            // read merged daily_transaction file line-by-line
+            BufferedReader bufferreader = new BufferedReader(new FileReader(mergedTransFile));
+            transaction = bufferreader.readLine();
+            
+            // handle function cals for each in operation
+            while (transaction != null) {     
+                String[] splitStr = transaction.split("\\s+");
+                System.out.println("tranaction: " + splitStr[0]);
+                if (splitStr[0] == "00") {
+                    break;
+                } else if (splitStr[0] == "01") {
+                    // create user
+                    userManager.createUser(transaction);
+                } else if (splitStr[0] == "02") {
+                    // delete user
+                    userManager.deleteUser(transaction, auction);
+                } else if (splitStr[0] == "03") {
+                    // advertise
+                    auction.advertise(transaction);
+                } else if (splitStr[0] == "04") {
+                    // bid
+                    auction.bid(transaction);
+                } else if (splitStr[0] == "05") {
+                    // refund
+                    userManager.refund(transaction);
+                } else if (splitStr[0] == "06") {
+                    // addcredit
+                    userManager.addCredit(transaction);
+                } else if (splitStr[0] == "07") {
+                    // need to create
+                    userManager.resetPassword(transaction);
+                    System.out.println(transaction);
+                }
+                
+                transaction = bufferreader.readLine();
+            }
+            bufferreader.close();
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        // subtract teh duration of each itme by one after each run
+        auction.endDay(userManager);
+        // wrirte to new accounts file and available items file
+        auction.write("new_available_items.txt");
+        userManager.write("new_current_user_accounts.txt");
+	}
 }
---
+++ PHASE-4/Backend/src/main/UserManager.java
@@ -0,0 +1,203 @@
+// File input (AuctionBay/PHASE-4/Backend/): daily_transaction.txt, available_items.txt, current_user_accounts.txt
+// File output (AuctionBay/PHASE-4/): new_available_items.txt, new_current_user_accounts.txt
+
+package main;
+
+import java.io.*; 
+import java.util.Vector;
+
+// UserManager Class -- handles each user operations line-by-line
+public class UserManager {
+	private Vector<User> users;
+
+    // constructor
+	public UserManager() {
+		users = new Vector<User>();
+	}
+	
+	// validate if string read is a double (for credit values)
+	public double validateDouble(String doubleStr) {
+        double creditD = 0;
+        try{
+            creditD = Double.parseDouble(doubleStr);
+        } catch(NumberFormatException ex) { // handle your exception
+            ex.printStackTrace();
+        }
+        return creditD;
+	}
+
+    // displays all users to console as a message
+    public void displayUsers() {
+        // * TO-DO: Fix console message. *
+        System.out.println("Printing all Users");
+        int i = 0;
+        while( i < users.size()) {
+            users.get(i).formatOutput();
+            i++;
+        }
+    }
+	
+	// loads the user objects from the file into the vector
+	public void load(String filename) {
+        // loop through the file creating a user object for each record and adding them to the vector
+        String userFile;
+        String usernameIn;
+        String accountTypeIn;
+        double creditAmountIn;
+        String password;
+
+        try {
+            BufferedReader bufferreader = new BufferedReader(new FileReader(filename));
+            userFile = bufferreader.readLine();
+
+            while (userFile != null) { 
+                String[] splitStr = userFile.split(" ");
+                usernameIn = splitStr[0];
+                accountTypeIn = splitStr[1];
+                creditAmountIn = validateDouble(splitStr[2]);
+                password = splitStr[3];
+                
+                User newUser = new User(usernameIn, accountTypeIn, creditAmountIn, password);
+                users.add(newUser);
+            
+                userFile = bufferreader.readLine();
+            }
+            bufferreader.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+	}
+	
+	// adds a newly created User to the vector
+	public void createUser(String transaction) {
+		// parses the transaction and creates a new user object and adds it to the vector
+		// String[] splitStr = transaction.split("\\s+");
+
+        String[] splitStr = transaction.split(" ");
+        String username = splitStr[1];
+        String accountType = splitStr[2];
+        Double credit = validateDouble(splitStr[3]);
+        String password = splitStr[4];
+
+        // * TO-DO: Check if new user created has a username that is different from all existing users. *
+
+        User createdUser = new User(username, accountType, credit, password);
+        users.add(createdUser);
+        // * TO-DO: Fix console message. *
+		System.out.println("User " + username + " has been created.");
+
+	}
+	
+	// finds and removes a User from the vector
+	public Auction deleteUser(String transaction, Auction auction) throws IOException {
+        // parse the transaction and removes the user object with a matching username from the vector
+        // String[] splitStr = transaction.split("\\s+");
+        String[] splitStr = transaction.split(" ");
+        String username = splitStr[1];
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUsername().equals(username)) {
+                users.remove(i);
+                // * TO-DO: Fix console message. *
+                System.out.println("User " + username + " has been deleted.");
+                break;
+            }
+        }
+        auction.deleteAllAuctions(username);
+        
+        return auction;
+	}
+	
+	// finds the 2 users and updates their credit amount
+	public void refund(String transaction) {
+        // parses the transaction then uses findUser to find both users then use the updateCredits for both users
+        // switching the sign where appropriate 
+        String[] splitStr = transaction.split(" ");
+
+        String buyerName = splitStr[1];
+        String sellerName = splitStr[2];
+        Double credit = validateDouble(splitStr[3]);
+        
+        // * TO-DO: Fix console message. *
+        System.out.println("refund amount " + credit);
+        for (int i = 0; i < users.size(); i++) {
+            // buyer
+            if (users.get(i).getUsername().equals(buyerName)) {
+                users.get(i).updateCreditAmount(credit);
+            } // seller
+            else if (users.get(i).getUsername().equals(sellerName)) {
+                users.get(i).updateCreditAmount(-credit);    
+            }
+        }
+
+        System.out.println("Refund transaction completed");
+	}
+	
+	// finds a user and updates their credit amount
+	public void addCredit(String transaction) {
+        // parse the transaction then use findUser to locate the correct user object then use updateCredit for 
+        // that user to add the credit 
+        String[] splitStr = transaction.split(" ");
+        String username = splitStr[1];
+        Double credit = validateDouble(splitStr[3]);
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUsername().equals(username)) {
+                users.get(i).updateCreditAmount(credit);
+                // * TO-DO: Fix console message. *
+                System.out.println(credit+" has been added to " + username);
+                break;
+            }
+        }
+	}
+
+    // finds a user and resets their password
+	public void resetPassword(String transaction) { 
+        // parse the transaction then use findUser to locate the correct user object then use resetPassword for that user 
+        String[] splitStr = transaction.split(" ");
+        String username = splitStr[1];
+        String password = splitStr[2];
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUsername().equals(username)) {
+                users.get(i).setPassword(password);
+                // * TO-DO: Fix console message. *
+                System.out.println(username + " has reset password " + password);
+                break;
+            }
+        }
+	}
+	
+	// transfers the funds from the buyer to the seller when the auction ends
+	public void closeAuction(String buyer, String seller, double amount) {
+        // use findUser to locate the buyer and seller object then use the updateCredit method for each object to 
+        // transfer the amount from the buyer to the seller
+        for (int i = 0; i < users.size(); i++) {
+            // add money to seller
+            if (users.get(i).getUsername().equals(seller)) {
+                    users.get(i).updateCreditAmount(amount);
+            }// subtract credit from buyer
+            else if (users.get(i).getUsername().equals(buyer)) {
+                    users.get(i).updateCreditAmount(amount*-1);
+            }
+        }
+        // * TO-DO: Fix console message. *
+        System.out.println("Auction closed");
+	}
+	
+	// writes the formated users to the output file
+	public void write(String filename) throws FileNotFoundException, IOException {
+        // loops through the vector and writes the formatOutput string from each user object to the file
+        PrintWriter writer = new PrintWriter(filename, "UTF-8");
+        int i = 0;
+        while( i < users.size()) {
+                writer.println(users.get(i).formatOutput());
+                i++;
+        }
+        writer.close();
+
+        // * TO-DO: Fix console message. *
+        System.out.println("All users have been written to file.");
+    }
+	// add findUser method that search for a user with a specific username and returns its index 
+}
